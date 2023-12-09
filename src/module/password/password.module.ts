c516164f409e8fc08bca78ea01560a335cb02762
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2023-12-09 00:24:09 -0300

    feat: Removing controller service on module password

--- src/module/password/password.module.ts
+++ src/module/password/password.module.ts
@@ -1,10 +1,9 @@
 import { PrismaService } from 'src/service/database/prisma.service';
-import { PasswordController } from './password.controller';
 import { PasswordService } from './password.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-  controllers: [PasswordController],
+  controllers: [],
   providers: [PasswordService, PrismaService],
 })
 export class PasswordModule {}
