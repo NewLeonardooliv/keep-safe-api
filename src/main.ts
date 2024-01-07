0d36445bceb7bfe48d58f7b42182cdaeb7ed5f7c
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-01-07 10:36:34 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './module/app.module';
 // import { ResponseA256GCMInterceptor } from './interceptors/responseA256GCM.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // app.useGlobalInterceptors(new ResponseA256GCMInterceptor());
 
   await app.listen(3000);
